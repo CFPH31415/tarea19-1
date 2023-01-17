a2be85eb3bd9a7d43b1dea0671ebd9911eecb781
Carlos Portillo <carlosportillo308@gmail.com>
2023-01-17 00:01:32 -0500

    api y con boton

---
+++ src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PokedexService } from './services/pokedex.service';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'tarea19';
+  data:any[] = [];
+  searchTerm!:string;
+  pokemons:any[]=[];
+
+  constructor(private pokedex:PokedexService){}
+
+  ngOnInit():void{
+    this.getPokemons()
+  }
+  searchPokemon() {
+    this.data = this.data.filter(pokemon => {
+        return pokemon.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+               pokemon.orden.toString().includes(this.searchTerm);
+    });
+}
+resetSearch() {
+  this.data = [...this.pokemons]; 
+  this.searchTerm = "";
+}
+    
+getPokemons(){
+  let pokemonData;
+  for(let i = 1; i < 150; i++){
+      this.pokedex.getPokemon(String(i)).subscribe(
+          res => {
+              pokemonData = {
+                  position: i,
+                  image: res.sprites.front_default,
+                  name: res.name,
+                  orden: res.order
+              }
+              this.data.push(pokemonData)
+              this.pokemons = [...this.data];
+          },
+          err => {
+          }
+      )
+  }
+}}
